27eaa7e510fb8379777d59becf20ce0af5c84bd4
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-12-12 05:49:40 +0300

    >>

--- fast_sort/test.rb
+++ fast_sort/test.rb
@@ -2,10 +2,10 @@
 
 puts
 
-['fixnum', 'flonum', 'noflonum'].each do | type |
+['fixnums', 'fix_1M', 'fix_1K', 'flonums', 'noflonums'].each do | type |
     ['sorted', '1'].each do | mode |
         ['../build/miniruby ', './'].each do | exec |
-            puts '$ ' + (cmd = "#{exec}#{type}s.rb #{mode}")
+            puts '$ ' + (cmd = "#{exec}#{type}.rb #{mode}")
             system(cmd) || raise
         end
     end
