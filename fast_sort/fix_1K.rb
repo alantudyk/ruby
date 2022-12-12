23e7d3dbaf04702f7ae8247f4921dff6b3ae531e
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-12-12 07:43:40 +0300

    >>

---
+++ fast_sort/fix_1K.rb
@@ -0,0 +1,22 @@
+#!/bin/ruby
+
+srand 0
+r = $*[0] == 'sorted' ? (-5_000_000...5_000_000).to_a :
+                        (Array.new 1e7.to_i do rand -2 ** 10...2 ** 10 end)
+
+puts
+
+5.times do
+
+    a = r.clone
+    t = Time.now
+    a.sort!
+    puts "\tRun ##{_1 + 1}: %.3f s" % [Time.now - t]
+
+end
+
+puts; exit unless $*[0]
+
+p (r.sort { _1 <=> _2 }) == (r.sort)
+
+puts
